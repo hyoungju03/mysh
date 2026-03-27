b87181d3fcdbbdbb5010c34cd65296b0819923d0
Hyoungju Lim <hyoungju03@gmail.com>
2026-03-27 21:24:01 +0900

    wip: added command parsing

--- src/main.c
+++ src/main.c
@@ -1,20 +1,27 @@
 #include <stdio.h>
 #include <limits.h>
+#include <string.h>
+#include <stdbool.h>
 
 int main() {
 
-    printf("The longest number of character input for stdin is: %d bytes.\n", LINE_MAX);
+    // printf("The longest number of character input for stdin is: %d bytes.\n", LINE_MAX);
     char input[LINE_MAX];
 
-    // read user input from stdin
-    fgets(input, LINE_MAX, stdin);
+    while (true) {
+        // read user input from stdin
+        // store it in a string buffer
+        // - how to manage their size, in case of long input?
+        //      - start with static sized string buffer
+        fgets(input, LINE_MAX, stdin);
 
-    printf("Location of string buffer: %p\n", input);
-    printf("The input string: %s\n", input);
-
-    // store it in a string buffer
-    // - how to manage their size, in case of long input?
-    //      - start with static sized string buffer
+        // with the input string, probably have to parse it into commands and arguments?
+        // need string manipulation
+        char *prog = strtok(input, " ");
+        if (prog != NULL) {
+            printf("Run the following program: %s", prog);
+        }
+    }
 
     return 0;
 }
