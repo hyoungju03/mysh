4831600b1fcb5b4343c3a50cc5c33c46442a3e7f
Hyoungju Lim <hyoungju03@gmail.com>
2026-08-15 01:00:11 -0400

    able to handle signals

--- src/main.c
+++ src/main.c
@@ -5,11 +5,16 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <libgen.h>
+#include <signal.h>
 
 #define CMD_DELIM " \n"
 
 
 int main() {
+
+	sigset_t set;
+	sigemptyset(&set);
+	sigaddset(&set, SIGTTOU);
 
     char input[LINE_MAX];
 
@@ -61,20 +66,64 @@ int main() {
 			continue;
 		}
 
+		int sync_pipe[2];
+		if (pipe(sync_pipe) < 0) {
+			perror("pipe failed");
+			exit(EXIT_FAILURE);
+		}
+
         pid_t pid = fork();
         switch (pid) {
+
             case -1:
-                perror("fork failed\n");
+                perror("Fork failed!\n");
+				exit(1);
+
             case 0:
-				// move child process into different process group
-				// setpgid(0, 0);
+				close(sync_pipe[1]);
+				char dummy;
+				if (read(sync_pipe[0], &dummy, 1) < 0) {
+					perror("child read failed");
+				}
+				close(sync_pipe[0]);
+
+				setpgid(0, 0);
 				// printf("Child PID: %ld, PGID: %ld\n", (long)getpid(), (long)getpgrp());
+				
                 execvp(file, argv);
 				exit(0);
+
             default:
-				// setpgid(0, 0);
+				// Close the read-end; the parent only writes to this pipe
+				close(sync_pipe[0]);
+
 				// printf("Parent PID: %ld, PGID: %ld\n", (long)getpid(), (long)getpgrp());
+				pid_t child_pid = pid;
+				char notification_byte = 'x';
+
+				// move child process into a new process group
+				setpgid(child_pid, child_pid);
+				// change the child process group as foreground process group of the session
+				if (tcsetpgrp(STDIN_FILENO, pid) < 0) {
+					printf("Changing foreground process group failed.\n");
+					exit(1);
+				}
+
+				// Housekeeping is complete. Write a byte to unblock the child.
+				if (write(sync_pipe[1], &notification_byte, 1) < 0) {
+					perror("parent write failed");
+				}
+				close(sync_pipe[1]);
+				
                 wait(NULL);
+				
+				sigprocmask(SIG_BLOCK, &set, NULL);
+				// gain back foreground process group
+				if (tcsetpgrp(STDIN_FILENO, getpgrp()) < 0) {
+					printf("Restoring foreground process group failed.\n");
+					exit(1);
+				}
+				sigprocmask(SIG_UNBLOCK, &set, NULL);
         }
     }
 
